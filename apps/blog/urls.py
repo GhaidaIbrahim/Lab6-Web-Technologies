3bff89a5051608c808bf5e80352790e479665888
Ghaida <Xghaidax34340@gmail.com>
2026-04-08 17:43:38 +0300

    Finish Lab 7: Django Models and ORM Queries

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
     path('search', views.search, name='books.search'),
+    path('add-book-now/', views.some_view_function, name='add_book_test'),
+    path('simple/query', views.simple_query),
+    path('complex/query', views.lookup_query),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Book
 def index(request):
     return render(request, "index.html") 
 def list_books(request):
@@ -9,13 +10,11 @@ def aboutus(request):
     return render(request, 'aboutus.html')
 def search(request):
     return render(request, 'blog/search.html')
-
 def __getBooksList():
     book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
     book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
     book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
     return [book1, book2, book3]
-
 def search(request):
     if request.method == "POST":
         string = request.POST.get('keyword').lower()
@@ -33,4 +32,18 @@ def search(request):
         
         return render(request, 'blog/bookList.html', {'books':newBooks})
 
+def some_view_function(request):
+    mybook = Book(title = 'Continuous Delivery', author = 'J.Humble and D. Farley', edition = 1)
+    mybook.save()
     return render(request, 'blog/search.html')
+def simple_query(request):
+    mybooks=Book.objects.filter(title__icontains='and') # <- multiple objects
+    return render(request, 'blog/bookList.html', {'books':mybooks})
+
+def lookup_query(request):
+    mybooks = Book.objects.filter(author__isnull=False).filter(title__icontains='and').filter(edition__gte=2).exclude(price__lte=100)[:10]
+    
+    if len(mybooks) >= 1:
+        return render(request, 'blog/bookList.html', {'books': mybooks})
+    else:
+        return render(request, 'index.html')
